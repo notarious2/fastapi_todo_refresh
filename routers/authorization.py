c1dcfc72d2f86462ece8da20f2d848360cb0da32
notarious1~git config --global user.email notarious2@gmail.comgit config --global user.name notarious <notarious2@gmail.com>
2022-11-17 18:05:06 +0500

    changed access token expiration to 1 day

--- routers/authorization.py
+++ routers/authorization.py
@@ -7,7 +7,7 @@ from password_hashing import Hash
 from fastapi_jwt_auth import AuthJWT
 from fastapi.encoders import jsonable_encoder
 import datetime
-expires = datetime.timedelta(minutes=1)
+expires = datetime.timedelta(days=1)
 router = APIRouter(
     tags = ['auth']
 )
